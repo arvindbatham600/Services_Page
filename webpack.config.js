e1366dbed7db76fba7c415c674674b1525682279
Arvind Batham <arvindbatham600.com>
2023-01-25 18:58:08 +0530

    change for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
+module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3002/",
+    publicPath:
+      argv.mode === "deployment"
+        ? "http://localhost:3002/"
+        : "https://services-page-eta.vercel.app/",
   },
 
   resolve: {
@@ -61,4 +64,4 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
-};
+});
